6e79820c251da58ec7d756f48cbeb4f6c8822d11
Michael <michaelgaitanas@gmail.com>
2023-03-27 10:50:26 +0300

    .

---
+++ examples/test.cpp
@@ -0,0 +1,100 @@
+#include"../imgui/imgui.h"
+#include"../imgui/imgui_impl_glfw.h"
+#include"../imgui/imgui_impl_opengl3.h"
+
+#include<GL/glew.h>
+#include<GLFW/glfw3.h>
+
+#include<cstdio>
+
+void raw_hardware_input(GLFWwindow *window)
+{
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+    {
+        glfwSetWindowShouldClose(window, true);
+    }
+    return;
+}
+
+void framebuffer_size_callback(GLFWwindow *window, int width, int height)
+{
+    glViewport(0,0,width,height);
+    return;
+}
+
+int main()
+{
+	glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    GLFWwindow *window = glfwCreateWindow(800,700, "Basic imgui io", NULL, NULL);
+    if (window == NULL)
+    {
+        printf("Failed to open a glfw window. Exiting...\n");
+        return 0;
+    }
+
+    glfwMakeContextCurrent(window);
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+    glewExperimental = GL_TRUE;
+    if (glewInit() != GLEW_OK)
+    {
+        printf("Failed to initialize glew. Exiting...\n");
+        return 0;
+    }
+
+    //initialize imgui
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    ImGuiIO &io = ImGui::GetIO();
+    io.IniFilename = NULL;
+    (void)io;
+    ImGui::StyleColorsDark();
+    ImGui_ImplGlfw_InitForOpenGL(window, true);
+    ImGui_ImplOpenGL3_Init("#version 330");
+
+    bool checked = false;
+
+    glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+    while (!glfwWindowShouldClose(window))
+    {
+		glClear(GL_COLOR_BUFFER_BIT);
+
+        //new frame
+        ImGui_ImplOpenGL3_NewFrame();
+        ImGui_ImplGlfw_NewFrame();
+        ImGui::NewFrame();
+
+        ImGui::Begin("Basic io");
+
+        ImGui::PushStyleColor(ImGuiCol_Button,ImVec4(0.0f,1.0f,0.0f,1.0f));
+        if (ImGui::Button("Toggle"))
+        {
+            checked = !checked; // toggle check state with Ctrl key
+        }
+        ImGui::PopStyleColor();
+        ImGui::SameLine();
+        ImGui::Text(checked ? "Checked" : "Unchecked");
+
+        ImGui::End();
+
+        ImGui::Render();
+        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
+        raw_hardware_input(window);
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    //free imgui resources
+    ImGui_ImplOpenGL3_Shutdown();
+    ImGui_ImplGlfw_Shutdown();
+    ImGui::DestroyContext();
+
+    glfwTerminate();
+
+	return 0;
+}
