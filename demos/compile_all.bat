808e707df09a71fd6bf7a77a8bc3f769e4a6ceb8
Michael <michaelgaitanas@gmail.com>
2024-12-05 23:40:14 +0200

    Updates

--- demos/compile_all.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-REM set FILES=demo1....cpp demo2....cpp, etc... and then : for %%f in (%FILES%) do (
-for %%f in (*.cpp) do (
-    g++ %%f -o %%~nf.exe -L "%USERPROFILE%\github\OpenGL_demos\imgui" -limgui -lopengl32 -lglew32 -lglfw3
-)
--- examples/compile_all.bat
+++ examples/compile_all.bat
@@ -1,5 +1,5 @@
 @echo off
 REM set FILES=ex1_black_window.cpp ex2_colored_window.cpp, etc... and then : for %%f in (%FILES%) do (
 for %%f in (*.cpp) do (
-    g++ %%f -o %%~nf.exe -L "%USERPROFILE%\github\OpenGL_demos\imgui" -limgui -lopengl32 -lglew32 -lglfw3
+    g++ %%f -o %%~nf.exe -L "%USERPROFILE%\github\OpenGL_demos\imgui" -limgui -lopengl32 -lglew32 -lglfw3 -Wall -Wpedantic -Wextra -Werror
 )
