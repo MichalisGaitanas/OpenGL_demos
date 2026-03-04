f4ce504dc16096a5e5dad40a98b8db2958f11585
Michael <michaelgaitanas@gmail.com>
2026-03-04 09:52:43 +0200

    Update test.cpp

--- demos/test.cpp
+++ demos/test.cpp
@@ -8,7 +8,7 @@ void forceDraw() {
     glFlush();
     // This tiny sleep is the "secret sauce" to break modern smoothing.
     // It gives the OS Compositor a window to grab a half-finished frame.
-    std::this_thread::sleep_for(std::chrono::milliseconds(2)); 
+    std::this_thread::sleep_for(std::chrono::milliseconds(1)); 
 }
 
 int main() {
@@ -17,10 +17,10 @@ int main() {
     // Explicitly disable double buffering
     glfwWindowHint(GLFW_DOUBLEBUFFER, GLFW_FALSE);
     
-    GLFWwindow* window = glfwCreateWindow(800, 600, "Single Buffer Visual Artifacts", NULL, NULL);
+    GLFWwindow* window = glfwCreateWindow(800, 600, "Single buffering", NULL, NULL);
     glfwMakeContextCurrent(window);
 
-    glClearColor(1.0f,1.0f,1.0f,1.0f);
+    glClearColor(0.0f,0.0f,0.0f,0.0f);
     while (!glfwWindowShouldClose(window)) {
         // 1. CLEAR: The screen goes black
         glClear(GL_COLOR_BUFFER_BIT);
@@ -34,7 +34,6 @@ int main() {
             glVertex2f(x - 0.4f, -0.4f);
             glVertex2f(x + 0.4f, -0.4f);
         glEnd();
-        forceDraw();
 
         // 3. DRAW PIECE 2: The rest of the triangle appears
         glBegin(GL_TRIANGLES);
